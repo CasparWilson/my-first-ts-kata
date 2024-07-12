fc49cac5585bedc4098badae7a55dab3ee2ae2bf
Caspar <casparwilson111@gmail.com>
2024-07-12 15:34:40 +0100

    3 functions written for typescript practice

--- src/skeleton.ts
+++ src/skeleton.ts
@@ -8,4 +8,16 @@ function sum(a: number, b: number): number {
     return a + b;
 }
 
+export function concatStringAndNum(str: string, num: number): [string, number]{
+    return [str, num]
+}
+
+export function findBigNumbers(numArray: number[]): number[]{
+    return numArray.filter((num)=>num > 1000)
+}
+
+export function findFirstBigNumber(numArray: number[]): number|null{
+    return numArray.find((num)=>num > 1000)??null
+}
+
 export { sum };
